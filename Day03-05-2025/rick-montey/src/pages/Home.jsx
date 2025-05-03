950e2e19ce9be5153982d1053a3e3a9c8786f6a8
Hemant142 <yhemant203@gmail.com>
2025-05-03 16:17:29 +0530

    Updated

--- Day03-05-2025/rick-montey/src/pages/Home.jsx
+++ Day03-05-2025/rick-montey/src/pages/Home.jsx
@@ -1,28 +1,53 @@
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import CharacterCard from "../components/CharacterCard";
 import Clock from "../components/Clock";
 import "./Home.css";
 
 export default function Home() {
-  const [characters, setCharacters] = useState([]);
+  const [allCharacters, setAllCharacters] = useState([]);
   const [page, setPage] = useState(1);
-  const [totalPages, setTotalPages] = useState(1);
+  const itemsPerPage = 6;
+  const navigate = useNavigate();
+
+  const handleRandomClick = () => {
+    const randomId = Math.floor(Math.random() * 826) + 1;
+    navigate(`/character/${randomId}`);
+  };
 
   useEffect(() => {
-    fetch(`https://rickandmortyapi.com/api/character?page=${page}`)
-      .then((res) => res.json())
-      .then((data) => {
-        setCharacters(data.results || []);
-        setTotalPages(data.info.pages);
-      });
-  }, [page]);
+    const fetchAllCharacters = async () => {
+      try {
+        let characters = [];
+        let nextUrl = "https://rickandmortyapi.com/api/character";
+        while (nextUrl) {
+          const res = await fetch(nextUrl);
+          const data = await res.json();
+          characters = characters.concat(data.results);
+          nextUrl = data.info.next;
+        }
+        setAllCharacters(characters);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    fetchAllCharacters();
+  }, []);
+
+  const totalPages = Math.ceil(allCharacters.length / itemsPerPage);
+  const startIndex = (page - 1) * itemsPerPage;
+  const selectedCharacters = allCharacters.slice(
+    startIndex,
+    startIndex + itemsPerPage
+  );
 
   return (
     <div className="home">
       <h1 style={{ color: "black" }}>Rick and Morty Encyclopedia</h1>
 
       <div className="grid">
-        {characters.map((char) => (
+        {selectedCharacters.map((char) => (
           <CharacterCard key={char.id} character={char} />
         ))}
       </div>
@@ -45,6 +70,10 @@ export default function Home() {
         >
           Next ➡
         </button>
+      </div>
+
+      <div className="random-character">
+        <button onClick={handleRandomClick}>Go to Random Character</button>
       </div>
 
       <footer className="detail-footer">
